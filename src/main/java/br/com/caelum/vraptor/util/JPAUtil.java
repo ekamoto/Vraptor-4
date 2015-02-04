52ce3d89e516a42b32078cf8ec3a24a3702500dd
Leandro Shindi Ekamoto <ekamoto.leandro@gmail.com>
2015-02-04 11:39:01 -0300

    Ajuste

--- src/main/java/br/com/caelum/vraptor/util/JPAUtil.java
+++ src/main/java/br/com/caelum/vraptor/util/JPAUtil.java
@@ -7,7 +7,6 @@ import javax.persistence.Persistence;
 public class JPAUtil {
 
 	public static EntityManager criaEntityManager() {
-		
 		EntityManagerFactory factory = Persistence
 				.createEntityManagerFactory("default");
 		return factory.createEntityManager();
